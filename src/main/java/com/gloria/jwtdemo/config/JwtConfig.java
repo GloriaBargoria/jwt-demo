2de29eda90e63ed7a7b229a607948d62b254654b
GloriaBargoria <bargoriagloria@gmail.com>
2022-01-22 14:09:58 +0300

    Configure authentication manager & httpsecurity

---
+++ src/main/java/com/gloria/jwtdemo/config/JwtConfig.java
@@ -0,0 +1,44 @@
+package com.gloria.jwtdemo.config;
+
+import com.gloria.jwtdemo.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class JwtConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailService);
+    }
+
+    //Control endpoints that are permitted or not
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .cors()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/generateToken").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    public PasswordEncoder PasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ src/main/java/com/gloria/jwtdemo/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.gloria.jwtdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello from Gloria your Queen";
+    }
+}
